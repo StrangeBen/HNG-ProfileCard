46e90f0c364f9ceb3585a44f42105fee418b0aa1
Oduola Ebenezer <oduolabenfico@gmail.com>
2025-01-31 15:20:00 +0100

    Made a couple of adjustments

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 /** @format */
+
 function updateTime() {
 	const now =
 		new Date();
@@ -12,3 +13,10 @@ function updateTime() {
 }
 
 updateTime();
+
+const bioText =
+	"I'm an aspiring frontend developer eager to learn and grow in the tech space. I enjoy building small projects to improve my skills and explore new technologies. Passionate about creating simple and user-friendly web experiences. I love sharing ideas, collaborating with others, and staying up-to-date with industry trends. My goal is to become a skilled developer who builds practical and impactful web solutions. Every day is an opportunity to get better and refine my craft.";
+document.getElementById(
+	"biography",
+).textContent =
+	bioText;
